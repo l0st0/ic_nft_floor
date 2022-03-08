734e7ce6b131c094d58c887ae4d14874203de881
l0st0 <43645635+l0st0@users.noreply.github.com>
2022-03-08 17:41:28 +0100

    clean up home route

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import Routes from './routes';
-import { Search } from './components/Search';
 import ScrollToTop from './components/ScrollToTop';
 import { useAppSelector } from './hooks';
 import { createTheme } from '@mui/material';
 import { getDesignTokens } from './styles';
-import { TopBar } from './components/TopBar';
 
 function App() {
   const { mode } = useAppSelector((state) => state.common);
@@ -23,8 +21,6 @@ function App() {
     <ThemeProvider theme={muiTheme}>
       <ScrollToTop />
       <CssBaseline />
-      <TopBar />
-      <Search />
       <Routes />
     </ThemeProvider>
   );
--- src/frontend/src/routes/Home.tsx
+++ src/frontend/src/routes/Home.tsx
@@ -2,6 +2,8 @@ import { Refresh } from '@mui/icons-material';
 import { LoadingButton } from '@mui/lab';
 import { Button, Stack } from '@mui/material';
 import { NftList } from '../components/NftList';
+import { Search } from '../components/Search';
+import { TopBar } from '../components/TopBar';
 import { TotalIcpValue } from '../components/TotalIcpValue';
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { getCollections } from '../store/slices/collection';
@@ -10,51 +12,49 @@ import { getData } from '../store/slices/data';
 export const Home = () => {
   const dispatch = useAppDispatch();
 
-  const { collections, error, principalID, validating } = useAppSelector((state) => state.collection);
+  const { collections, error: collectionError, principalID, validating } = useAppSelector((state) => state.collection);
   const { error: dataError } = useAppSelector((state) => state.data);
 
-  if (dataError) {
-    return (
-      <Stack alignItems='center' spacing={2}>
-        <span>There was some error when fetching data</span>
-        <Button onClick={() => dispatch(getData({}))}>Fetch again</Button>
-      </Stack>
-    );
-  }
+  const displayCollections = !dataError && !collectionError && !!collections.length;
 
-  if (error && principalID.length) {
-    return (
-      <Stack alignItems='center' spacing={2}>
-        {error ? <span>There was some error when getting collections</span> : <span>We found no collections :/</span>}
-
-        <LoadingButton
-          loading={validating}
-          startIcon={<Refresh />}
-          loadingPosition='start'
-          onClick={() => dispatch(getCollections({ principalID, validate: true }))}
-        >
-          {validating ? 'Trying to fetch' : 'Try again'}
-        </LoadingButton>
-      </Stack>
-    );
-  }
-
-  if (!collections.length && !principalID.length) return null;
-  // return (
-  //   <Stack alignItems='center' spacing={2}>
-  //     <span>There might be some problem with fetching collections because of subnet.</span>
-  //   </Stack>
-  // );
-
-  return !!collections.length ? (
+  return (
     <>
-      <TotalIcpValue />
-      <NftList />
+      <TopBar />
+      <Search />
+
+      {dataError && (
+        <Stack alignItems='center' spacing={2}>
+          <span>There was some error when fetching data</span>
+          <Button onClick={() => dispatch(getData({}))}>Fetch again</Button>
+        </Stack>
+      )}
+
+      {collectionError && (
+        <Stack alignItems='center' spacing={2}>
+          <span>There was some error when getting collections</span>
+
+          <LoadingButton
+            loading={validating}
+            startIcon={<Refresh />}
+            loadingPosition='start'
+            onClick={() => dispatch(getCollections({ principalID, validate: true }))}
+          >
+            {validating ? 'Trying to fetch' : 'Try again'}
+          </LoadingButton>
+        </Stack>
+      )}
+
+      {displayCollections ? (
+        <>
+          <TotalIcpValue />
+          <NftList />
+        </>
+      ) : (
+        <Stack alignItems='center' spacing={2}>
+          <span>No collection found.</span>
+        </Stack>
+      )}
     </>
-  ) : (
-    <Stack alignItems='center' spacing={2}>
-      No collection found.
-    </Stack>
   );
 };
 
